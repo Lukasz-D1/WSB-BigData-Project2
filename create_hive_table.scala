9d42b8011dd3ec6a39be5bfe74bf9e981084cf5a
Lukasz Dobek <lukasz.dobek@outlook.com>
2021-01-11 22:27:04 +0100

    Fix hive create table scripts

--- create_hive_table.scala
+++ create_hive_table.scala
@@ -1,11 +1,11 @@
 spark.sql("show tables").show() 
 
-spark.sql("""CREATE TABLE `w_czas` ( 
-    `data` date, 
+spark.sql("""CREATE TABLE IF NOT EXISTS `w_czas` ( 
+    `data` timestamp, 
     `dzien` int,
     `miesiac` int, 
     `rok` int,
-    `godzina` int,) 
+    `godzina` int) 
 ROW FORMAT SERDE 
     'org.apache.hadoop.hive.ql.io.orc.OrcSerde'
 STORED AS INPUTFORMAT
@@ -13,7 +13,7 @@ STORED AS INPUTFORMAT
  OUTPUTFORMAT 
     'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'""")
 
-spark.sql("""CREATE TABLE `w_pogoda` ( 
+spark.sql("""CREATE TABLE IF NOT EXISTS `w_pogoda` ( 
     `id_pogody` int, 
     `warunki_pogodowe` string) 
 ROW FORMAT SERDE 
@@ -23,7 +23,7 @@ STORED AS INPUTFORMAT
  OUTPUTFORMAT 
     'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'""")
 
-spark.sql("""CREATE TABLE `w_geografia` ( 
+spark.sql("""CREATE TABLE IF NOT EXISTS `w_geografia` ( 
     `id_jednostki_adm` string, 
     `nazwa` string,
     `kod_regionu` string,
@@ -35,7 +35,7 @@ STORED AS INPUTFORMAT
  OUTPUTFORMAT 
     'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'""")
 
-spark.sql("""CREATE TABLE `w_drogi` ( 
+spark.sql("""CREATE TABLE IF NOT EXISTS `w_drogi` ( 
     `id_drogi` int, 
     `typ_drogi` string,
     `kategoria_drogi` string) 
@@ -46,7 +46,7 @@ STORED AS INPUTFORMAT
  OUTPUTFORMAT 
     'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'""")
 
-spark.sql("""CREATE TABLE `w_typ_pojazdu` ( 
+spark.sql("""CREATE TABLE IF NOT EXISTS `w_typ_pojazdu` ( 
     `id_typu_pojazdu` int, 
     `typ_pojazdu` string,
     `kategoria_pojazdu` string,
@@ -58,4 +58,17 @@ STORED AS INPUTFORMAT
  OUTPUTFORMAT 
     'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'""")
 
-spark.sql("show tables").show() 
+spark.sql(""" CREATE TABLE IF NOT EXISTS `f_fakty` (
+    `data` date,
+    `id_pogody` int,
+    `id_jednostki_adm` string,
+    `id_drogi` int,
+    `id_typu_pojazdu` int,
+    `liczba_pojazdow` int)
+ROW FORMAT SERDE 
+    'org.apache.hadoop.hive.ql.io.orc.OrcSerde'
+STORED AS INPUTFORMAT
+    'org.apache.hadoop.hive.ql.io.orc.OrcInputFormat'
+ OUTPUTFORMAT 
+    'org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat'""")
+
